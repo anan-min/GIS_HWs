284c43767912660baade82b7336e8d9f114a6ad2
nut <nut999anan@hotmail.com>
2025-05-23 09:12:54 +0700

    i4 update

--- src/components/Intermediate/i4/i4.component.ts
+++ src/components/Intermediate/i4/i4.component.ts
@@ -1,13 +1,14 @@
-import { Attribute, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import { AfterViewInit } from '@angular/core';
 import Graphic from '@arcgis/core/Graphic';
-import SimpleFillSymbol from '@arcgis/core/symbols/SimpleFillSymbol';
 import { SimpleMarkerSymbol } from '@arcgis/core/symbols';
 import SimpleLineSymbol from '@arcgis/core/symbols/SimpleLineSymbol';
 import FeatureSet from '@arcgis/core/rest/support/FeatureSet';
 import { CommonModule } from '@angular/common';
+import * as route from '@arcgis/core/rest/route';
+import RouteParameters from '@arcgis/core/rest/support/RouteParameters.js';
 
 @Component({
   selector: 'app-i2',
@@ -50,18 +51,22 @@ export class I4Component implements AfterViewInit {
 
   handleMapClick(event: any): void {
     const clickedPoint = event.mapPoint;
+
     if (this.StartLocation === null) {
       this.StartLocation = clickedPoint;
       this.markPoint(clickedPoint, true);
     } else {
-      this.locationList.push(clickedPoint);
       this.markPoint(clickedPoint, false);
-      console.log(this.locationList);
+    }
+
+    if (this.locationList.length > 1) {
+      this.requestRoute();
     }
   }
 
   markPoint(location: any, isFirst: boolean): void {
     let style: 'circle' | 'square' = isFirst ? 'square' : 'circle';
+    console.log('location', location);
 
     const marker = new Graphic({
       geometry: location,
@@ -71,21 +76,39 @@ export class I4Component implements AfterViewInit {
         style: style,
       }),
       attributes: {
-        Name: '',
+        Name: `Location ${this.locationList.length + 1}`,
       },
     });
     this.mapView.graphics.add(marker);
+    this.locationList.push(marker);
   }
 
   handleClearAllClick(): void {
     this.StartLocation = null;
     this.locationList = [];
     this.mapView.graphics.removeAll();
-
     this.isStart = false;
   }
 
   handleStartClick(): void {
     this.isStart = true;
+  }
+
+  requestRoute(): void {
+    const routeParams = new RouteParameters({
+      stops: new FeatureSet({
+        features: [...this.locationList],
+      }),
+      returnDirections: true,
+    });
+
+    route
+      .solve(
+        'https://sampleserver6.arcgisonline.com/arcgis/rest/services/NetworkAnalysis/SanDiego/NAServer/Route',
+        routeParams
+      )
+      .then((response) => {
+        console.log(response.routeResults[0].route);
+      });
   }
 }
