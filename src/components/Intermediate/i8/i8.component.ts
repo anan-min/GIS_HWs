c6971723afe0cce2224f1849c1bda4b1f02120a6
nut <nut999anan@hotmail.com>
2025-05-23 15:16:24 +0700

    working on validations

--- src/components/Intermediate/i8/i8.component.ts
+++ src/components/Intermediate/i8/i8.component.ts
@@ -5,12 +5,14 @@ import { AfterViewInit } from '@angular/core';
 import Graphic from '@arcgis/core/Graphic';
 import { SimpleMarkerSymbol } from '@arcgis/core/symbols';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import Point from '@arcgis/core/geometry/Point';
 
 @Component({
   selector: 'app-i8',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './i8.component.html',
   styleUrls: ['./i8.component.css'],
 })
@@ -21,14 +23,52 @@ export class I8Component implements AfterViewInit {
 
   public lattitude: number = 0;
   public longitude: number = 0;
+  private currentUser: any = null;
 
-  public users: any[] = [];
+  public users: any[] = [
+    {
+      name: 'สมชาย1',
+      surname: 'ศรีประเสริฐ',
+      gender: 'M',
+      mobile: '0912345678',
+      lattitude: 13.7563, // Latitude for Bangkok
+      longitude: 100.5018, // Longitude for Bangkok
+    },
+    {
+      name: 'สมชาย2',
+      surname: 'ศรีประเสริฐ',
+      gender: 'F',
+      mobile: '0912345679',
+      lattitude: 14.5995, // Latitude for Chiang Mai
+      longitude: 98.6793, // Longitude for Chiang Mai
+    },
+    {
+      name: 'สมชาย3',
+      surname: 'ศรีประเสริฐ',
+      gender: 'M',
+      mobile: '0912345680',
+      lattitude: 15.87, // Latitude for Krabi
+      longitude: 98.293, // Longitude for Krabi
+    },
+  ];
 
   public userForm = new FormGroup({
-    name: new FormControl('นัท'),
-    surname: new FormControl('อนันต์'),
-    gender: new FormControl('ชาย'),
-    mobile: new FormControl('0888605503'),
+    name: new FormControl('นัท', [
+      Validators.required, // Name is required
+      Validators.minLength(2), // Name should be at least 2 characters
+    ]),
+    surname: new FormControl('อนันต์', [
+      Validators.required, // Surname is required
+      Validators.minLength(2), // Surname should be at least 2 characters
+    ]),
+    gender: new FormControl('M', [
+      Validators.required, // Gender is required
+      Validators.pattern('^(M|F)$'), // Gender must be 'M' or 'F'
+    ]),
+    mobile: new FormControl('0888605503', [
+      Validators.required, // Mobile is required
+      Validators.pattern('^0[0-9]{9}$'), // Mobile must be 10 digits starting with 0
+    ]),
   });
 
   ngAfterViewInit(): void {
@@ -85,8 +125,69 @@ export class I8Component implements AfterViewInit {
     this.mapView.graphics.add(marker);
   }
 
-  onClearFormClick(): void {}
-  onUserEditClick(user: any): void {}
-  onAddUserClick(): void {}
-  onSaveFormClick(): void {}
+  onClearFormClick(): void {
+    this.userForm.reset();
+    this.isAddingUser = true;
+    this.isEditingUser = false;
+  }
+
+  onUserEditClick(user: any): void {
+    this.currentUser = user;
+    this.userForm.patchValue({
+      name: user.name,
+      surname: user.surname,
+      gender: user.gender,
+      mobile: user.mobile,
+    });
+    this.goToUserLocation(user);
+  }
+
+  onAddUserClick(): void {
+    this.isAddingUser = true;
+    this.isEditingUser = false;
+    this.userForm.reset();
+    const defaultLocation = new Point({
+      latitude: 13.7563,
+      longitude: 100.5018,
+    });
+    this.mapView.goTo(defaultLocation);
+    this.markPoint(defaultLocation);
+  }
+  onSaveFormClick(): void {
+    this.currentUser = {
+      name: this.userForm.value.name,
+      surname: this.userForm.value.surname,
+      gender: this.userForm.value.gender,
+      mobile: this.userForm.value.mobile,
+      lattitude: this.lattitude,
+      longitude: this.longitude,
+    };
+    console.log(this.currentUser);
+    this.userForm.reset();
+    this.isAddingUser = true;
+    this.isEditingUser = false;
+  }
+
+  goToUserLocation(user: any): void {
+    const location = new Point({
+      latitude: user.lattitude,
+      longitude: user.longitude,
+    });
+
+    const marker = new Graphic({
+      geometry: location,
+      symbol: new SimpleMarkerSymbol({
+        color: [218, 156, 115, 0.7],
+        size: 5,
+        style: 'circle',
+      }),
+      attributes: {
+        Name: `test`,
+      },
+    });
+
+    this.mapView.graphics.removeAll();
+    this.mapView.graphics.add(marker);
+    this.mapView.goTo(location);
+  }
 }
